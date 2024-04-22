3076aaff6ba025a4ee9d7d805c8c6811159334af
Gabriel Menoni <contato.gmenoni@gmail.com>
2024-04-21 22:23:16 -0300

    Polished

--- Misc/generator.py
+++
@@ -1,9 +0,0 @@
-terms = open('terms.txt', 'w')
-
-with open ('dicionario.txt', 'r') as dicionario:
-    for linha in dicionario:
-        if len(linha) == 6:
-            terms.write(linha.lower())
-dicionario.close()
-
-terms.close()
